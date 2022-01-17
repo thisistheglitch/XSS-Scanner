6fa2062b517bde8d868f6f6fb7c489105e7b1f1d
thisistheglitch <97638363+thisistheglitch@users.noreply.github.com>
2022-01-17 20:50:20 +0100

    Update xss.py
    Add save file in '/file'

--- xss.py
+++ xss.py
@@ -5,9 +5,21 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from random import randint
 
 class XssScanner:
-    def __init__(self, url, wordlist, methode):
+    def __init__(self, url, wordlist, methode, proxy):
+        if(proxy == False):
+            print('No proxy')
+        elif(proxy == "API"):
+            print('API PROXY SELECTED')
+            self.proxyon = True
+            self.proxy = 'api'
+        else:
+            print('Proxy true')
+            self.proxyon = True
+            self.proxy = proxy
+
         self.methode = methode
         self.wordlist = wordlist
         self.url = url
@@ -21,6 +33,21 @@ class XssScanner:
         except:
             print(Fore.RED +"Error reply.")
 
+    def close_browser(self):
+        print('Close Browser...')
+        self.driver.close()
+        quit()
+
+    def save_result(self):
+        filename = randint(1, 1000000)
+        files = open('files/last_'+str(filename)+'_result.txt', "a")
+        files.write(self.result)
+        for item in self.data_result:
+                files.write(str(item))
+        print('File save in files/last_'+str(filename)+'_result.txt')
+        files.close()
+        self.close_browser()
+
     def run(self):
         self.data_result = []
         print(Fore.GREEN +'URL attack : '+self.url+'')
@@ -28,9 +55,10 @@ class XssScanner:
             print(Fore.GREEN +'Wordlist : '+self.wordlist)
         else:
             print(Fore.RED +'Wordlist does not exist : '+self.wordlist)
-            quit()
+            self.close_browser()
         self.count = 0
         self.detect = 0
+        
         with open(self.wordlist, 'r') as f:
             for i, line in enumerate(f):
                 if self.methode == 'get' or self.methode == 'GET':
@@ -44,6 +72,7 @@ class XssScanner:
                         pass
                     self.count = self.count +1
 
+                    #print(Fore.GREEN+'GET : '+self.url+str(line))
                     try:
                         WebDriverWait(self.driver, 0).until (EC.alert_is_present())
                         #WebDriverWait(driver, 5).until (EC.alert_is_present())
@@ -54,17 +83,28 @@ class XssScanner:
                         self.data_result.extend([self.url+str(line)])
                     except TimeoutException:
                         print(Fore.RED + 'No XSS detected | '+self.url+str(line) + 'URL receive --->'+self.driver.current_url+'\n')
+
+
                 elif self.methode == 'post' or self.methode == 'POST':
-                    print('Construct')
-                    quit()                    
+                    print('Method not supported')
+                    self.close_browser()                 
                 pass
         print("######SCAN_END####")
         if self.detect >= 1:
             print(Fore.GREEN +'DETECTED\n('+str(self.detect)+'/'+str(self.count)+')')
+            self.result = 'URL DETECTED ('+str(self.detect)+'/'+str(self.count)+')\n'
+
             for item in self.data_result:
                 print(str(item))
-
+            #self.result = 'URL DETECTED\n '+ str(self.data_result) +'('+str(self.detect)+'/'+str(self.count)+')'
+            save = input('Save scan result ?')
+            if save == "yes" or save == "y":
+                self.save_result()
+            else:
+                self.close_browser()
+            #print(self.data_result)
         else:
-            print(Fore.RED+'No detected.')
+            print(Fore.RED+'No XSS detected.')
+            self.close_browser()
 
 
