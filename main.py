a4eb1e3e71346570781321e771f0403ff408b93b
thisistheglitch <97638363+thisistheglitch@users.noreply.github.com>
2022-01-17 20:51:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 from threading import Thread
 from xss import XssScanner
 def __main():
-    url = 'http://localhost/xsstest.php?q=' 
+    url = 'http://192.168.1.14/xsstest.php?q=' 
     wordlist = "wordlist/low.txt"
     methode = 'get'
     thread = 2
-    lunch = XssScanner(url, wordlist, methode)
+    proxy = False
+    lunch = XssScanner(url, wordlist, methode, proxy)
     lunch.run()
 print(" _   _     _       _       _   _                  _ _       _     \n| |_| |__ (_)___  (_)___  | |_| |__   ___    __ _(_| |_ ___| |__  \n| __| '_ \| / __| | / __| | __| '_ \ / _ \  / _` | | __/ __| '_ \ \n| |_| | | | \__ \ | \__ \ | |_| | | |  __/ | (_| | | || (__| | | |\n \__|_| |_|_|___/ |_|___/  \__|_| |_|\___|  \__, |_|\__\___|_| |_|\n                                            |___/                 \n")
+
 __main()
